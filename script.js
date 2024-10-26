8e5a6f4a11f392eac213ac7726eac30ff72c3f3f
Koichi Nakayamada <151496024+koichincom@users.noreply.github.com>
2024-10-26 15:04:05 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function startChat() {
-    document.getElementById("welcome-screen").style.display = "none";
-    document.getElementById("chat-screen").style.display = "flex";
-}
-
-function stopChat() {
-    document.getElementById("welcome-screen").style.display = "flex";
-    document.getElementById("chat-screen").style.display = "none";
-}
